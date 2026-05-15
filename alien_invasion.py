e79b18a246eebccee38e05c807c9edaa69800e67
dashenwkp <2126879457@qq.com>
2026-05-15 19:21:42 +0800

    舰队移动功能实现

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -96,6 +97,19 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _check_fleet_edges(self):
+        '''在有外星人到达边缘时采取相应的措施'''
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        '''将整个外星舰队向下移动, 并改变它们的方向'''
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         '''更新屏幕上的图像，并切换到新屏幕'''
         self.screen.fill(self.settings.bg_color)
@@ -115,6 +129,11 @@ class AlienInvasion:
         for bullet in self.bulltes.copy():
             if bullet.rect.bottom <= 0:
                 self.bulltes.remove(bullet)
+
+    def _update_aliens(self):
+        '''检查是否有外星人位于屏幕边缘, 并更新整个外星舰队的位置'''
+        self._check_fleet_edges()
+        self.aliens.update()
 
 if __name__ == '__main__':
     '''创建游戏实例并运行游戏'''
